601412ca4da3baf2b91fc08eaf9f10f4eef32b81
George Pashnev <100297574+George60229@users.noreply.github.com>
2022-12-29 11:48:40 +0200

    Update 2.sql

--- «13-PL-SQL-Trigger»/2.sql
+++ «13-PL-SQL-Trigger»/2.sql
@@ -16,7 +16,7 @@ END;
 -- додавання значення FK-колонки, що не існує у PK-колонці. Навести тест-кейс перевірки
 -- роботи тригера.
 
-CREATE OR REPLACE TRIGGER user_insert_check
+CREATE OR REPLACE TRIGGER user_insert_check_this
     BEFORE INSERT
     ON car
     FOR EACH ROW
@@ -35,10 +35,9 @@ END;
 insert into car (car_id,user_id,make,mode_car) values(1005,100000,'Volfswagen','passat')
 
 /*
-Error starting at line : 35 in command -
 insert into car (car_id,user_id,make,mode_car) values(1005,100000,'Volfswagen','passat')
 Error report -
-ORA-20551: Manager with id=100000 NOT exist!!!
-ORA-06512: at "PASHNYEV.CLIENT_INSERT_CHECK_MANAGERID_VALID", line 10
-ORA-04088: error during execution of trigger 'PASHNYEV.CLIENT_INSERT_CHECK_MANAGERID_VALID'
+ORA-20551: No user with id=100000
+ORA-06512: at "PASHNYEV.USER_INSERT_CHECK_THIS", line 10
+ORA-04088: error during execution of trigger 'PASHNYEV.USER_INSERT_CHECK_THIS'
 */
